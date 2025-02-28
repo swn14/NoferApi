bf3c25e2a6d3e506ee796aa4df1c4672ba4f42ce
Sheldon Nofer <snofer@bethel.jw.org>
2025-02-28 08:32:58 -0500

    Add airport properties

--- NoferApi.Domain/Airports/Airport.cs
+++ NoferApi.Domain/Airports/Airport.cs
@@ -4,5 +4,28 @@ namespace NoferApi.Domain.Airports;
 
 public class Airport : Entity
 {
-    
+    public string Type { get; private set; }
+    public string Name { get; private set; }
+    public double Latitude { get; private set; }
+    public double Longitude { get; private set; }
+    public double ElevationFeet { get; private set; }
+    public string Continent { get; private set; }
+    public string IsoCountry { get; private set; }
+    public string IsoRegion { get; private set; }
+    public string Municipality { get; private set; }
+    public string ScheduledService { get; private set; }
+    public string GpsCode { get; private set; }
+    public string IcaoCode { get; private set; }
+    public string IatoCode { get; private set; }
+    public string LocalCode { get; private set; }
+    public string WebsiteLink { get; private set; }
+    public string WikipediaLink { get; private set; }
+    public HashSet<string> Keywords { get; private set; } = new HashSet<string>();
+
+    private Airport(Action<Airport> setterAction)
+    {
+        setterAction(this);
+    }
+
+    public static Airport Create(Action<Airport> setterAction) => new(setterAction);
 }
